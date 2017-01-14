6c67a4f1d86f24097293c79a4c27ef6a33869bf9
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-01-13 23:22:44 -0600

    Adding cucumber support
    BDD testing engine

---
+++ src/main/resources/service/GenerationServiceTest.java
@@ -0,0 +1,116 @@
+/*
+ *  
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Daniel Cortes Pichardo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package mx.infotec.dads.kukulkan.engine.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.metamodel.DataContext;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.data.domain.Example;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import mx.infotec.dads.kukulkan.engine.domain.core.DataModelContext;
+import mx.infotec.dads.kukulkan.engine.domain.core.DataModelGroup;
+import mx.infotec.dads.kukulkan.engine.domain.core.DataStore;
+import mx.infotec.dads.kukulkan.engine.domain.core.GeneratorContext;
+import mx.infotec.dads.kukulkan.engine.domain.core.JavaDataModelContext;
+import mx.infotec.dads.kukulkan.engine.domain.core.ProjectConfiguration;
+import mx.infotec.dads.kukulkan.engine.domain.core.Rule;
+import mx.infotec.dads.kukulkan.engine.domain.core.RuleType;
+import mx.infotec.dads.kukulkan.engine.factories.LayerTaskFactory;
+import mx.infotec.dads.kukulkan.engine.repository.RuleRepository;
+import mx.infotec.dads.kukulkan.util.ArchetypeType;
+import mx.infotec.dads.kukulkan.util.DataMapping;
+import mx.infotec.dads.kukulkan.util.InflectorProcessor;
+
+/**
+ * Test for GeneratorService
+ * 
+ * @author Daniel Cortes Pichardo
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DirtiesContext
+public class GenerationServiceTest {
+
+    @Autowired
+    private GenerationService generationService;
+    @Autowired
+    private DataStoreService dataStoreService;
+    @Autowired
+    private RuleRepository ruleRepository;
+    @Autowired
+    private LayerTaskFactory layerTaskFactory;
+
+    @Test
+    public void generationService() throws Exception {
+        Rule rule = new Rule();
+        RuleType ruleType = new RuleType();
+        ruleType.setId(1);// singular
+        rule.setRuleType(ruleType);
+        Example<Rule> ruleExample = Example.of(rule);
+        List<Rule> rulesList = ruleRepository.findAll(ruleExample);
+        for (Rule item : rulesList) {
+            System.out.println(item.getReplacement());
+            InflectorProcessor.getInstance().addSingularize(item.getExpression(), item.getReplacement());
+        }
+        // Create ProjectConfiguration
+        ProjectConfiguration pConf = new ProjectConfiguration();
+        pConf.setId("gen");
+        pConf.setGroupId("mx.infotec.dads.innovation");
+        pConf.setVersion("1.0.0");
+        pConf.setPackaging("mx.infotec.dads.innovation.zonacero");
+        pConf.setYear("2017");
+        pConf.setAuthor("KUKULKAN");
+        pConf.setWebLayerName("rest");
+        pConf.setServiceLayerName("service");
+        pConf.setDaoLayerName("repository");
+        pConf.setDomainLayerName("model");
+        // Create DataStore
+        DataStore dataStore = dataStoreService.getDataStore(1l);
+        // Create DataModel
+        DataModelContext dmCtx = new JavaDataModelContext(dataStore);
+        // Create DataContext
+        DataContext dataContext = dataStoreService.getDataContext(dataStore);
+        dmCtx.setDataContext(dataContext);
+        // Tables to process
+        List<String> tablesToProcess = new ArrayList<>();
+        // Mapping DataContext into DataModel
+        List<DataModelGroup> dmgList = DataMapping.createSingleDataModelGroupList(dmCtx.getDataContext(),
+                tablesToProcess);
+        dmCtx.setDataModelGroup(dmgList);
+        // Create GeneratorContext
+        GeneratorContext genCtx = new GeneratorContext(dmCtx, pConf);
+        // Process Activities
+        generationService.process(genCtx, layerTaskFactory.getLayerTaskSet(ArchetypeType.REST_SPRING_JPA));
+
+    }
+}
