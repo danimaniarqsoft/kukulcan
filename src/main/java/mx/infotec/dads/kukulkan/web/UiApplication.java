4a12375fab2f51cd5120ad2fbe3ddbc24df07065
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-01-19 00:12:56 -0600

    documenting

--- src/main/java/mx/infotec/dads/kukulkan/web/UiApplication.java
+++ src/main/java/mx/infotec/dads/kukulkan/web/UiApplication.java
@@ -5,10 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,7 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class UiApplication {
 
     // Match everything without a suffix (so not a static resource)
-    @RequestMapping(value = "/{path:[^\\.]*}")
+//    @RequestMapping(value = "/{path:[^\\.]*}")
     public String redirect() {
         // Forward to home page so that route is preserved.
         return "forward:/";
