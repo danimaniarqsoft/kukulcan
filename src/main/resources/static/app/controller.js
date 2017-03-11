d4ca5e616191eb5ebd753b97cdbe75f53b342e3a
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-03-11 00:23:07 -0600

    refactoring views
    refactoring views

---
+++ src/main/resources/static/app/controller.js
@@ -0,0 +1,41 @@
+app.controller('dashboardController', function($scope) {
+	$scope.headingTitle = "DashboardList";
+});
+
+app.controller('Hello', function($scope, $http) {
+	$http.get('https://jsonplaceholder.typicode.com/posts/1').then(
+		function(response) {
+			$scope.greeting = response.data;
+		});
+});
+
+
+app.controller('generatorController', function($scope, $http) {
+	$scope.dataStores = [];
+	$http.get('http://localhost:8080/dataStores').then(function(response) {
+		$scope.dataStores = response.data;
+	});
+	
+	$http.get('http://localhost:8080/generateApplication/newContext').then(function(response) {
+		$scope.context = response.data;
+	});
+	
+	$scope.submit = function() {		
+		$http.post('http://localhost:8080/generateApplication', $scope.context).then(function(response) {
+			console.log("ok");
+		}, function(response) {
+			console.log(response);
+		});
+	};
+	
+	$scope.file_changed = function(element) {
+	     $scope.$apply(function(scope) {
+	         var photofile = element.files[0];
+	         var reader = new FileReader();
+	         reader.onload = function(e) {
+	            // handle onload
+	         };
+	         reader.readAsDataURL(photofile);
+	     });
+	};
+});
